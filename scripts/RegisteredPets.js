f698356485d2e4fabec1999b5d8dd7133873b514
Helen Esterman <helenesterman99@gmail.com>
2023-07-31 14:28:57 -0400

    added eventListener to pets module to show which pets are being walked by who when clicked

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,28 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
+
+document.addEventListener(
+    "click",
+    (theActualClick) => {
+        const clickTarget = theActualClick.target
+        const walkerId = clickTarget.dataset.walkerkey
+        const allWalkers = getWalkers()
+
+        for (const walker of allWalkers) {
+            if(walker.id === parseInt(walkerId)){
+                window.alert(`This pet is being walked by ${walker.name}`)
+            }
+        }
+
+    }
+)
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li data-walkerKey="${pet.walkerId}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
