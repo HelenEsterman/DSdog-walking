d05c2b425bec30d83bc1f230f1e674a97ce7b7c0
Helen Esterman <helenesterman99@gmail.com>
2023-07-31 12:17:47 -0400

    added eventListener in walkers module to show city of walker when clicked on

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -2,12 +2,22 @@ import { getWalkers } from "./database.js"
 
 const walkers = getWalkers()
 
+document.addEventListener(
+    "click",
+    (theClick) => {
+        const whatWeClicked = theClick.target
+        
+        if(whatWeClicked.dataset.type === "walker"){
+        window.alert(`This walker works in ${whatWeClicked.dataset.city}`)
+        }
+    }
+)
 
 export const Walkers = () => {
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
-        walkerHTML += `<li data-id="${walker.id}" data-city="${walker.city}">${walker.name}</li>`
+        walkerHTML += `<li data-id="${walker.id}" data-city="${walker.city}" data-type="walker">${walker.name}</li>`
     }
 
     return walkerHTML += "</ul>"
